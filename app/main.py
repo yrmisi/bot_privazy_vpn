6418ddf008789119cb78bc1b74fef94c5e829e86
yrmisi <yrmisi@example.com>
2026-03-28 02:02:51 +0300

    create main

---
+++ app/main.py
@@ -0,0 +1,34 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from config import settings
+from database import session_pool
+from handlers.default import echo_router, start_router
+from middlewares import DbSessionMiddleware
+
+
+async def main() -> None:
+
+    dp = Dispatcher()
+
+    dp.update.middleware(DbSessionMiddleware(session_pool))
+
+    dp.include_routers(
+        start_router,
+        echo_router,
+    )
+    bot = Bot(
+        token=settings.bot.token.get_secret_value(),
+        default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+    )
+
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    asyncio.run(main())
