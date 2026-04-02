45d2d8f4d29d89fc9fd4cacb0a0e7f190999f15c
yrmisi <yrmisi@example.com>
2026-04-03 01:11:17 +0300

    create UserData

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,5 +1,7 @@
+from .user_answer import UserAnswer
 from .user_data import UserData
 
 __all__ = [
     "UserData",
+    "UserAnswer",
 ]
--- app/schemas/user_data.py
+++ app/schemas/user_data.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from datetime import datetime
 
 
 @dataclass
@@ -10,3 +11,4 @@ class UserData:
     is_bot: bool
     language_code: str
     is_premium: bool
+    free_trial: datetime
