b122ffea1e3b3f3247c9bb8cd96f448d92541b64
yrmisi <yrmisi@example.com>
2026-04-07 21:05:33 +0300

    create MessageCallbackConfig

--- app/config/bot.py
+++ app/config/bot.py
@@ -6,29 +6,14 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from .paths import ENVS_DIR
 
 
-class MessageUserUnauthConfig:
-    """Message config for unauthenticated user."""
-
-    key_text: str = "hello_unauth_user_text"
-    key_call_data: str = "hello_unauth_user_call_data"
-    rows_size: list[int] = [1]
-
-
-class MessageUserAuthConfig:
-    """Message config for authenticated user."""
-
-    key_text: str = "hello_auth_user_text"
-    key_call_data: str = "hello_auth_user_call_data"
-    rows_size: list[int] = [1, 2, 2, 1]
-    key_text_echo: str = "message_echo"
-
-
 class BotConfig(BaseSettings):
+    """
+    Bot configuration settings.
+    """
+
     token: Annotated[SecretStr, Field(alias="BOT_TOKEN")] = SecretStr("")
     local_file_type: str = ".json"
     free_days: int = 3
-    msg_unauth: MessageUserUnauthConfig = MessageUserUnauthConfig()
-    msg_auth: MessageUserAuthConfig = MessageUserAuthConfig()
 
     model_config = SettingsConfigDict(
         env_file=ENVS_DIR / ".env.bot",
