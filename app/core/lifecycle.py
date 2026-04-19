82179fe2bf798038fa7b9e9ea7a328419182d912
yrmisi <yrmisi@example.com>
2026-04-19 23:23:36 +0300

    add on_startup and on_shutdown

---
+++ app/core/lifecycle.py
@@ -0,0 +1,24 @@
+import logging
+
+from aiogram import Bot
+
+from config import settings
+from core.i18n import load_translations_to_cache
+from database import async_engine
+
+
+async def on_startup(bot: Bot) -> None:
+    """
+    Initialize data when starting the bot.
+    """
+    load_translations_to_cache()
+    logging.info("Translations loaded to cache.")
+
+
+async def on_shutdown(bot: Bot) -> None:
+    """
+    Correct termination: closing the connection.
+    """
+    if settings.bot.session:
+        await settings.bot.session.close()
+    await async_engine.dispose()
