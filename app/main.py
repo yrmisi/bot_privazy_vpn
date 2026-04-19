1f10247ed07ed05834907e858d267e66dbf50f3c
yrmisi <yrmisi@example.com>
2026-04-19 23:35:31 +0300

    add register startup and shutdown

--- app/main.py
+++ app/main.py
@@ -4,12 +4,13 @@ import logging
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
 from aiogram.enums import ParseMode
+from aiohttp_socks import ProxyConnectionError, ProxyError
 
 from config import settings
-from core.i18n import load_translations_to_cache
+from core.lifecycle import on_shutdown, on_startup
 from handlers.custom import free_router
 from handlers.default import echo_router, start_router
-from middlewares import include_middleware
+from middlewares import include_middlewares
 
 
 async def main() -> None:
@@ -18,20 +19,31 @@ async def main() -> None:
     """
     dp = Dispatcher()
 
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
+    include_middlewares(dp)
+
     dp.include_routers(
         start_router,
         free_router,
         echo_router,
     )
-    await include_middleware(dp)
 
     bot = Bot(
         token=settings.bot.token.get_secret_value(),
         default=DefaultBotProperties(parse_mode=ParseMode.HTML),
+        session=settings.bot.session,
     )
-    load_translations_to_cache()
 
-    await dp.start_polling(bot)
+    try:
+        await dp.start_polling(bot)
+    except ProxyConnectionError as exc:
+        logging.critical(f"Proxy connection error: {exc}")
+    except ProxyError as exc:
+        logging.critical(f"Proxy error: {exc}")
+    except Exception as exc:
+        logging.critical(f"Unknown error: {exc}")
 
 
 if __name__ == "__main__":
